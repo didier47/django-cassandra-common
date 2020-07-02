20e669193c294644ab5a052334f00d3452aa8521
didiergaona <didiergaona@wposs.com>
2020-07-02 16:15:59 -0500

    mejorando setup.py

--- setup.py
+++ setup.py
@@ -8,5 +8,20 @@ setup(
     install_requires=["Django>=3.0.6", 'djangorestframework>=3.11.0', 'cassandra-driver>=3.23.0', 'requests>=2.23.0',
                       'django-filter>=2.3.0', 'setuptools>=46.4.0', 'django-cassandra-engine>=1.6.1'],
     package_dir={"": "src"},
-    packages=find_namespace_packages(where="src")
+    packages=find_namespace_packages(where="src"),
+    url='https://github.com/didier47/django-cassandra-common',
+    description='Common classes I have found useful to develop with Django and Cassandra',
+    license='GNU General Public License v3.0',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: GNU General Public License v3.0 License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
 )
